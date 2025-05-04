23ddf45f32acdcd09cc999d4a34702dacfdba10e
Fedorich <panchev_fedor@mail.ru>
2025-05-05 02:38:50 +0300

    l-26.00

---
+++ src/components/navbar/NavbarButton.tsx
@@ -0,0 +1,17 @@
+import { ComponentProps, ReactNode } from 'react';
+
+type Props = {
+  aria?: string;
+  className?: string;
+  children: ReactNode;
+} & ComponentProps<'button'>;
+
+export function NavbarButton({className, children, aria}: Props) {
+  return  <button
+    className={`cursor-pointer p-2 rounded-full border-2 border-transparent hover:border-soft-bg transition-all duration-400 ${className}`}
+    type="button"
+    aria-label={aria}
+  >
+    {children}
+  </button>
+};
